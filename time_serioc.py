3776b0cc2d5a7e7f73ab4659907bf42ea4f398e7
ShrivardhanPatil09 <shripatil1769@gmail.com>
2025-06-25 17:37:41 +0530

    Add files via upload

---
+++ time_serioc.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Example data (Time Series)
+data = [30, 31, 29, 30, 75, 31, 32]
+df = pd.DataFrame(data, columns=['temperature'])
+
+# Moving Average
+df['rolling_mean'] = df['temperature'].rolling(window=3, center=True).mean()
+
+# Anomaly detection
+df['anomaly'] = abs(df['temperature'] - df['rolling_mean']) > 10
+
+print(df)
+
+# Plot
+plt.plot(df['temperature'], label='Temperature')
+plt.plot(df['rolling_mean'], label='Rolling Mean', linestyle='--')
+plt.scatter(df.index[df['anomaly']], df['temperature'][df['anomaly']], color='red', label='Anomaly')
+plt.legend()
+plt.title("Anomaly Detection using Moving Average")
+plt.show()
